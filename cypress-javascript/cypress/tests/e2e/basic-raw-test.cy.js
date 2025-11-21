06b5c2531f3a4848d399c065406ef7b0c8fa16f6
orhunakkan <orhunakkan@outlook.com>
2025-11-21 06:18:58 -0600

    chore: update dependencies across projects
    - cypress-javascript:
      - updated dotenv from 17.2.1 to 17.2.3
      - updated cypress from 15.0.0 to 15.7.0
      - updated @faker-js/faker from 10.0.0 to 10.1.0
      - updated mochawesome from 7.1.3 to 7.1.4
    
    - playwright-typescript:
      - updated @types/node from 24.3.0 to 24.10.1
      - updated @typescript-eslint/parser from 8.11.0 to 8.47.0
      - updated eslint from 9.12.0 to 9.39.1
    
    - selenium-javascript:
      - updated axios from 1.11.0 to 1.13.2
      - updated dotenv from 17.2.1 to 17.2.3
      - updated selenium-webdriver from 4.35.0 to 4.38.0
      - updated @types/selenium-webdriver from 4.1.28 to 4.35.4
      - updated @faker-js/faker from 10.0.0 to 10.1.0
      - updated chai from 6.0.1 to 6.2.1
      - updated mocha from 11.7.1 to 11.7.5
      - updated mochawesome from 7.1.3 to 7.1.4
    
    - removed .mocharc.cjs file from selenium-javascript
    - removed restful-booker-api-flow.js and basic-raw-test.spec.js test files

--- cypress-javascript/cypress/tests/e2e/basic-raw-test.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Homepage URL validation', () => {
-  it('should go to homepage and verify its URL', () => {
-    cy.visit('https://bonigarcia.dev/selenium-webdriver-java/index.html');
-    cy.title().should('eq', 'Hands-On Selenium WebDriver with Java');
-  });
-})
