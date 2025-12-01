ac362f07449010da6176160454d08855d1bcea7a
orhunakkan <orhunakkan@outlook.com>
2025-11-30 22:44:13 -0600

    style: Format function declarations and expressions for consistency; add arrow function examples

--- javascript-learning/code/02-Fundamentals-Part-2/starter/script.js
+++ javascript-learning/code/02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function logger () {
+function logger() {
     console.log('THIS IS ORHUN');
 }
 
@@ -8,7 +8,7 @@ function logger () {
 logger();
 
 
-function fruitProcessor (apples, oranges) {
+function fruitProcessor(apples, oranges) {
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
     return juice;
 }
@@ -16,8 +16,8 @@ function fruitProcessor (apples, oranges) {
 const myJuice = fruitProcessor(5, 10);
 console.log(myJuice);
 
-// Function Declaration
-function calcage1 (birthYear) {
+// Function Declarations
+function calcage1(birthYear) {
     const age = 2025 - birthYear;
     return age;
 }
@@ -25,14 +25,57 @@ function calcage1 (birthYear) {
 const age1 = calcage1(1990);
 console.log(age1);
 
-// Function Expression -> with this you need to create the functions at the top so that you can call them later.
+// Function Expressions -> with this you need to create the functions at the top so that you can call them later.
 
-const calcage2  = function (birthYear) {
+const calcage2 = function (birthYear) {
     return 2025 - birthYear;
 }
 
 const age2 = calcage2(1990);
 
 console.log(age1, age2);
+
+// Arrow functions
+
+const calcAge3 = birthYear => 2025 - birthYear;
+const age3 = calcAge3(1990);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear) => {
+    const age = calcAge3(birthYear);
+    const retirement = 65 - age;
+    return retirement;
+}
+
+console.log(yearsUntilRetirement(1990));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
