1c911ff95a579e0b788f0ae1882b85424f28aa97
orhunakkan <orhunakkan@outlook.com>
2025-12-05 00:20:42 -0600

    refactor: Improve constructor formatting in HomePage class for consistency

--- cypress-javascript/cypress/pages/homepage.js
+++ cypress-javascript/cypress/pages/homepage.js
@@ -1,13 +1,12 @@
 class HomePage {
-    constructor() {
-        // Locators
-        this.chapter3Card = 'Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over';
-        this.chapter4Card = 'Chapter 4. Browser-Agnostic Features Long page Infinite scroll Shadow DOM';
-        this.chapter5Card = 'Chapter 5. Browser-Specific Manipulation Geolocation Notifications Get user';
-        this.chapter7Card = 'Chapter 7. The Page Object Model (POM) Login form Slow login';
-        this.chapter8Card = 'Chapter 8. Testing Framework Specifics Random calculator';
-        this.chapter9Card = 'Chapter 9. Third-Party Integrations Download files A/B Testing Data types';
-    }
+  constructor() {
+    this.chapter3Card = 'Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over';
+    this.chapter4Card = 'Chapter 4. Browser-Agnostic Features Long page Infinite scroll Shadow DOM';
+    this.chapter5Card = 'Chapter 5. Browser-Specific Manipulation Geolocation Notifications Get user';
+    this.chapter7Card = 'Chapter 7. The Page Object Model (POM) Login form Slow login';
+    this.chapter8Card = 'Chapter 8. Testing Framework Specifics Random calculator';
+    this.chapter9Card = 'Chapter 9. Third-Party Integrations Download files A/B Testing Data types';
+  }
 }
 
 export default new HomePage();
