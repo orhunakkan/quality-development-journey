95037c2be15cd621ade0fab977912bb53e47d2c6
orhunakkan <orhunakkan@outlook.com>
2025-12-12 15:13:39 -0600

    feat: Add Lombok annotations to Note and User models for reduced boilerplate code

--- java-learning/demo/src/test/java/com/example/demo/models/Note.java
+++ java-learning/demo/src/test/java/com/example/demo/models/Note.java
@@ -1,5 +1,12 @@
 package com.example.demo.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Note {
     private String id;
     private String title;
@@ -10,76 +17,9 @@ public class Note {
     private String updated_at;
     private String user_id;
 
-    public Note() {
-    }
-
     public Note(String title, String description, String category) {
         this.title = title;
         this.description = description;
         this.category = category;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Boolean getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(Boolean completed) {
-        this.completed = completed;
-    }
-
-    public String getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(String created_at) {
-        this.created_at = created_at;
-    }
-
-    public String getUpdated_at() {
-        return updated_at;
-    }
-
-    public void setUpdated_at(String updated_at) {
-        this.updated_at = updated_at;
-    }
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
     }
 }
