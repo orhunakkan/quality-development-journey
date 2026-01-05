4305713b4e1f2a739a3b76763c098ac397ee814f
orhunakkan <orhunakkan@outlook.com>
2026-01-05 01:02:16 -0600

    feat: Add temperature amplitude calculation and related comments in script.js

--- javascript-learning/code/03-Developer-Skills/starter/script.js
+++ javascript-learning/code/03-Developer-Skills/starter/script.js
@@ -1,2 +1,29 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+
+// PROBLEM:
+// We work for a company building a smart home thermometer.
+// Our most recent task is this:
+// "Given an array of temperatures of one day, calculate the temperature amplitude.
+// Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// What to do for this problem
+// How to ignore errors
+// Find max value
+// Find min value
+// Subtract min form max and return it
+
+function calculateAmplitude(temperatureArr) {
+    const cleanedArr = [];
+    for (let i = 0; i < temperatureArr.length; i++) {
+        if (typeof temperatureArr[i] !== 'string') {
+            cleanedArr.push(temperatureArr[i]);
+        }
+    }
+    return (Math.max(...cleanedArr) - Math.min(...cleanedArr));
+}
+
+console.log(calculateAmplitude(temperatures));
+
