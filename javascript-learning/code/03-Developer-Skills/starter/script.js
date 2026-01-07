4d4c1cfcbfc3b1bd5e9e7d9606f14cac071e8866
orhunakkan <orhunakkan@outlook.com>
2026-01-07 12:56:51 -0600

    feat: Implement printForecast function to display temperature forecast

--- javascript-learning/code/03-Developer-Skills/starter/script.js
+++ javascript-learning/code/03-Developer-Skills/starter/script.js
@@ -27,3 +27,62 @@ function calculateAmplitude(temperatureArr) {
 
 console.log(calculateAmplitude(temperatures));
 
+// Problem 2:
+// Function should now receive 2 arrays of temperatures
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? No! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
+// - Send merged array to amplitude function
+
+// const temps1 = [3, 5, 1, 9];
+// const temps2 = [4, 0, -3, 12];
+// const temps3 = temps1.concat(temps2);
+
+// const measureKelvin = function () {
+//     const measurement = {
+//         type: 'temp',
+//         unit: 'celcius',
+//         value: prompt('Degrees Celcius:')
+//     }
+
+//     const kelvin = parseInt(measurement.value) + 273;
+//     return kelvin;
+// }
+
+// console.log(measureKelvin());
+
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures,
+the thermometer displays a string with these
+temperatures.
+
+Example: [17, 21, 23] will print "... 17°C in 1 days ...
+21°C in 2 days ... 23°C in 3 days ..."
+
+Create a function 'printForecast' which takes in an
+array 'arr' and logs a string like the above to the
+console.
+
+Use the problem-solving framework: Understand the
+problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+const testData1 = [17, 21, 23];
+const testData2 = [12, 5, -5, 0, 4];
+
+function printForecast (arr) {
+    let wholeString = '';
+    for (let i = 0; i < arr.length; i++) {
+        wholeString += `... ${arr[i]}°C in ${i+1} days `;
+    }
+}
+
+console.log(printForecast(testData2));
